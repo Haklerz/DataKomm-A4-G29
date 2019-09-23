ffeb51fb7b1167564e3bb241a99f119058133a67
Haklerz <haakon.lervik@gmail.com>
2019-09-23 11:14:15 +0200

    Started rewrite

--- Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpClientHandler.java
+++ Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpClientHandler.java
@@ -8,21 +8,16 @@ import java.util.Scanner;
 public class SimpleTcpClientHandler implements Runnable {
     private Socket clientSocket;
     private boolean finishedHandling;
+    private Scanner inFromClient;
+    private PrintWriter outToClient;
 
     public SimpleTcpClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
     }
 
-    public void run() {
+    public void ærun() {
         log("Client Connected");
-        Scanner inFromClient = null;
-        PrintWriter outToClient = null;
-        try {
-            inFromClient = new Scanner(this.clientSocket.getInputStream());
-            outToClient = new PrintWriter(this.clientSocket.getOutputStream(), true);
-        } catch (IOException e) {
-            log("ERROR: Connection to client has been lost");
-        }
+        
         if (inFromClient != null && outToClient != null) {
             while (inFromClient.hasNextLine() && !finishedHandling) {
                 String request = inFromClient.nextLine();
@@ -47,9 +42,30 @@ public class SimpleTcpClientHandler implements Runnable {
             } catch (IOException e) {
                 log("ERROR: An I/O error occured when closing connection");
             }
-            
         }
         log("Client disconnected");
+    }
+
+    public void run() {
+        openStreams();
+        if (streamsAreOpen()) {
+            while (inFromClient.hasNextLine()) {
+                String clientRequest = inFromClient.nextLine();
+            }
+        }
+    }
+
+    private boolean streamsAreOpen() {
+		return false;
+	}
+
+    private void openStreams() {
+        try {
+            inFromClient = new Scanner(this.clientSocket.getInputStream());
+            outToClient = new PrintWriter(this.clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            log("ERROR: Connection to client has been lost");
+        }
     }
 
     /**
