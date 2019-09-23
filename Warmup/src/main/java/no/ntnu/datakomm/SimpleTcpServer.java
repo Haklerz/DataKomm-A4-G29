fc32155483943207b8554ca48ceba3993d86838f
Haklerz <haakon.lervik@gmail.com>
2019-09-23 11:13:50 +0200

    Cleanup

--- Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpServer.java
+++ Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpServer.java
@@ -9,10 +9,21 @@ import java.util.concurrent.Executors;
  * A Simple TCP client, used as a warm-up exercise for assignment A4.
  */
 public class SimpleTcpServer {
+    // The max number of threads
+    private static final int THREAD_COUNT = 32;
+    // The TCP port
+    private static final int PORT = 1301;
     // The welcoming socket
     private ServerSocket serverSocket;
     // The thread pool
     private ExecutorService threadPool;
+
+    /**
+     * Instanziates a new server.
+     */
+    public SimpleTcpServer() {
+        this.threadPool = Executors.newFixedThreadPool(THREAD_COUNT);
+    }
 
     public static void main(String[] args) {
         SimpleTcpServer server = new SimpleTcpServer();
@@ -21,20 +32,59 @@ public class SimpleTcpServer {
         log("ERROR: the server should never go out of the run() method! After handling one client");
     }
 
+    /**
+     * Runs the server.
+     */
     public void run() {
-        threadPool = Executors.newFixedThreadPool(18);
+        openServer();
+        while (serverIsOpen())
+            acceptClient();
+        closeServer();
+    }
+
+    /**
+     * Returns wether the server is open.
+     * 
+     * @return wether the server is open
+     */
+    private boolean serverIsOpen() {
+        return !serverSocket.isClosed();
+    }
+
+    /**
+     * Closes the server socket.
+     */
+    private void closeServer() {
         try {
-            serverSocket = new ServerSocket(1301);
+            log("Closing server");
+            serverSocket.close();
+        } catch (IOException e) {
+            log("ERROR: An I/O error occured when closing server");
+        }
+    }
+
+    /**
+     * Accepts a client and handles the client in a seperate thread. Method blocks
+     * and waits until a client connects.
+     */
+    private void acceptClient() {
+        try {
+            log("Waiting for new client to connect");
+            threadPool.execute(new SimpleTcpClientHandler(serverSocket.accept()));
+        } catch (IOException e) {
+            log("ERROR: An I/O error occured while waiting for client");
+        }
+    }
+
+    /**
+     * Opens the server socket to a port.
+     */
+    private void openServer() {
+        try {
+            serverSocket = new ServerSocket(PORT);
+            log("Server opened on port: " + PORT);
         } catch (IOException e) {
             log("ERROR: An I/O error occured when opening server");
-        }
-        while (true) {
-            try {
-                log("Waiting for new client to connect");
-                threadPool.execute(new SimpleTcpClientHandler(serverSocket.accept()));
-            } catch (IOException e) {
-                log("ERROR: An I/O error occured while waiting for client");
-            }
         }
     }
 
