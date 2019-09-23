b5f5a646658e1d7262f009c02adfc27454522ac9
Haklerz <haakon.lervik@gmail.com>
2019-09-23 21:39:18 +0200

    Covering edge cases

--- Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpClientHandler.java
+++ Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpClientHandler.java
@@ -15,29 +15,43 @@ public class SimpleTcpClientHandler implements Runnable {
     }
 
     public void run() {
+        log("Client connected");
         openStreams();
-        boolean finishedHandling = false;
-        while (streamsAreOpen() && inFromClient.hasNextLine() && !finishedHandling) {
+        while (streamsAreOpen() && inFromClient.hasNextLine()) {
             String request = inFromClient.nextLine();
             if ("game over".equals(request)) {
-                finishedHandling = true;
+                closeStreams();
             } else {
                 String response = "error";
                 String[] numberStrings = request.split("\\+");
                 if (numberStrings.length == 2) {
                     try {
-                        response = "" + (Integer.parseInt(numberStrings[0]) + Integer.parseInt(numberStrings[1]));
-                    } finally {}
+                        response = "" + (Integer.parseInt(numberStrings[0].trim()) + Integer.parseInt(numberStrings[1].trim()));
+                    } catch (NumberFormatException e) {
+                    }
                 }
                 outToClient.println(response);
+                log("Responded to request < " + request + " > with < " + response + " >");
             }
         }
-        closeStreams();
+
+        closeSocket();
+        log("Client disconnected");
+    }
+
+    private void closeSocket() {
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            log("ERROR: An I/O error occured when closing socket");
+        }
     }
 
     private void closeStreams() {
         inFromClient.close();
         outToClient.close();
+        inFromClient = null;
+        outToClient = null;
     }
 
     private boolean streamsAreOpen() {
